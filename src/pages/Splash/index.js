53a8527ff2ff7be0c5149237c8fdfe4dbd4d638f
Sakura Wallace <066sakura@gmail.com>
2025-05-26 01:37:04 +0100

    created splash page

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SplashAnimation from './SplashAnimation';
+
+export default function Splash() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <SplashAnimation />
+        </div>
+    );
+}
