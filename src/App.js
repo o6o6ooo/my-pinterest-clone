1adb42934aa7547b6b80748c04c66ded0e6e3e1d
Sakura Wallace <066sakura@gmail.com>
2025-05-26 23:16:58 +0100

    changed search to gallery

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Splash from './pages/Splash';
 import InvitationCode from './pages/InvitationCode';
 import Auth from './pages/Auth';
 import HomeFeed from './pages/Homefeed';
-import Search from './pages/Search';
+import Gallery from './pages/Gallery';
 import Upload from './pages/Upload';
 import Notification from './pages/Notification';
 import User from './pages/User';
@@ -20,7 +20,7 @@ function App() {
           <Route path="/invite" element={<InvitationCode />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/home" element={<HomeFeed />} />
-          <Route path="/search" element={<Search />} />
+          <Route path="/Gallery" element={<Gallery />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/notifications" element={<Notification />} />
           <Route path="/user" element={<User />} />
