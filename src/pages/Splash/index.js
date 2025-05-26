73801a6737cd733a3349ea210ada2f00b960bc03
Sakura Wallace <066sakura@gmail.com>
2025-05-26 20:49:54 +0100

    enabled to show splash every time open the app

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import SplashAnimation from './SplashAnimation';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 export default function Splash() {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // 2秒間スプラッシュを表示してからチェック
+        const timer = setTimeout(() => {
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    navigate('/home');
+                } else {
+                    navigate('/auth');
+                }
+            });
+        }, 2000); // 2秒間表示
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
+
     return (
         <div className="flex justify-center items-center h-screen bg-[#A5C3DE]">
             <SplashAnimation />
