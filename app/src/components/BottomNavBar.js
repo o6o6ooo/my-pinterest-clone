5bc516df71e2ec5d495ed4d00142afb5a4737e47
Sakura Wallace <066sakura@gmail.com>
2025-06-03 19:53:55 +0100

    deleted gallery

--- app/src/components/BottomNavBar.js
+++ app/src/components/BottomNavBar.js
@@ -13,17 +13,6 @@ const HomeIcon = ({ filled, className }) => filled ? (
     </svg>
   );
 
-// Gallery
-const GalleryIcon = ({ filled, className }) => filled ? (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
-        <path fillRule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clipRule="evenodd" />
-    </svg>
-) : (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-    </svg>
-  );
-
 // Upload
 const UploadIcon = ({ filled, className }) => filled ? (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
@@ -62,14 +51,13 @@ const UserIcon = ({ filled, className }) => filled ? (
 export default function BottomNavBar({ onUploadClick }) { // onUploadClick受け取る
     const tabs = [
         { name: 'Home', to: '/home', Icon: HomeIcon },
-        { name: 'Gallery', to: '/gallery', Icon: GalleryIcon },
         { name: 'Notifications', to: '/notifications', Icon: NotificationsIcon },
         { name: 'User', to: '/user', Icon: UserIcon },
     ];
 
     return (
-        <nav className="fixed bottom-0 left-0 right-0 flex justify-between px-10 py-4 z-50 md:hidden bg-[#A5C3DE]" style={{ height: '100px' }}>
-            {/* home, gallery */}
+        <nav className="fixed bottom-0 left-0 right-0 flex justify-evenly px-4 py-4 z-50 md:hidden bg-[#A5C3DE]" style={{ height: '100px' }}>
+            {/* home */}
             {tabs.slice(0, 2).map(({ name, to, Icon }) => (
                 <NavLink
                     key={name}
--- app/src/pages/Gallery/index.js
+++
@@ -1,7 +0,0 @@
-export default function Gallery() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-[#A5C3DE] text-[#0A4A6E]">
-            <h1 className="text-xl font-semibold mb-4">Gallery</h1>
-        </div>
-    );
-}
