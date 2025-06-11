0bfcc7f46dbaccd711a49c8a5f4181f52b66cb2f
Sakura Wallace <066sakura@gmail.com>
2025-06-11 21:55:54 +0100

    applied camera emoji instead icon

--- app/src/components/UploadOverlay.js
+++ app/src/components/UploadOverlay.js
@@ -33,8 +33,7 @@ export default function UploadOverlay({ isOpen, onClose }) {
             onClick={handleOverlayClick}
         >
             <div
-                className="fixed bg-white rounded-xl shadow-lg flex flex-col items-center p-6 border border-[#0A4A6E]"
-                style={{ height: '20%', paddingTop: '50px' }} 
+                className="fixed bg-white rounded-xl shadow-lg flex flex-col items-center justify-center p-6 border border-[#0A4A6E] h-[15%] relative"
                 onClick={(e) => e.stopPropagation()}
             >
                 {/* close button*/}
@@ -53,21 +52,9 @@ export default function UploadOverlay({ isOpen, onClose }) {
                         onClick={() => document.getElementById("file-upload").click()}
                         className="flex flex-col items-center text-[#0A4A6E]"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="w-12 h-12"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                        <p className="mt-2 text-sm font-semibold">upload photos?</p>
+                        <p className="text-3xl">📸</p>
+                        <p className="text-sm font-semibold">upload photos?</p>
                     </button>
-
                     <input
                         id="file-upload"
                         type="file"
